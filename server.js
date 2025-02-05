89dba4141223ecc317ba5b623aa1633bd00dc445
Gift Mbonani <giftmbonani@outlook.com>
2025-02-05 06:07:26 -0800

    users

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 import express from 'express';
-import router from './routes/index';
+import path from 'path';
+import router from './routes/index.js';
+import dbClient from './utils/db.js';
 
 const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use('/', router);
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
-});
+(async () => {
+  try {
+    
+    await dbClient.connect();
+    console.log('Connected to MongoDB');
+    
+    app.use('/', router);
+    app.listen(port, () => {
+      console.log(`server running on port ${port}`);
+    });
+  } catch (error) {
+    console.error(' Could not connect to MongoDB:', error);
+    process.exit(1);
+  }
+})();
 
 export default app;
+
--- utils/db.js
+++ utils/db.js
@@ -9,23 +9,34 @@ const url = `mongodb://${HOST}:${PORT}`;
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
-    this.client.connect().then(() => {
-      this.db = this.client.db(`${DATABASE}`);
-    }).catch((err) => {
-      console.log(err);
-    });
+    this.db = null; // Initially set db to null
   }
 
+  // Async method to connect to the database
+  async connect() {
+    try {
+      await this.client.connect();
+      console.log('Connected to MongoDB');
+      this.db = this.client.db(DATABASE); // Set db once connected
+    } catch (err) {
+      console.log(' MongoDB Connection Error:', err);
+      throw err;
+    }
+  }
+
+  // Check if the client is connected
   isAlive() {
-    return this.client.isConnected();
+    return this.client.isConnected(); 
   }
 
+  // Get number of users
   async nbUsers() {
     const users = this.db.collection('users');
     const usersNum = await users.countDocuments();
     return usersNum;
   }
 
+  // Get number of files
   async nbFiles() {
     const files = this.db.collection('files');
     const filesNum = await files.countDocuments();
@@ -34,4 +45,5 @@ class DBClient {
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
+
